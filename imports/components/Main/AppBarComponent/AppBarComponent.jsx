b3cd0ca8ec0bf76975d0132978df4e39e0dd3556
Avogardo <avogardo0@gmail.com>
2017-11-19 15:53:29 +0100

    fix bug with refreshing chart tile after open option dialog

--- imports/components/Main/AppBarComponent/AppBarComponent.jsx
+++ imports/components/Main/AppBarComponent/AppBarComponent.jsx
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Menu from './Menu';
+import MenuDialog from './Menu/MenuDialog';
+
 import { AppBar } from 'material-ui';
 
 
-const AppBarComponent = ({ openDialog }) => (
-  <AppBar
-    title="System wizualizacji"
-    iconElementRight={<Menu openDialog={openDialog} />}
-  />
-);
+class AppBarComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.openDialog = this.openDialog.bind(this);
+    this.hideDialog = this.hideDialog.bind(this);
+
+    this.state = {
+      showMenuDialog: false,
+    };
+  }
+
+  openDialog() {
+    this.setState({
+      showMenuDialog: true,
+    });
+  }
+
+  hideDialog() {
+    this.setState({
+      showMenuDialog: false,
+    });
+  }
+
+  render() {
+    const { showMenuDialog } = this.state;
+
+    return (
+      <div>
+        <AppBar
+          title="System wizualizacji"
+          iconElementRight={<Menu openDialog={this.openDialog} />}
+        />
+
+        <MenuDialog
+          open={showMenuDialog}
+          onClose={this.hideDialog}
+        />
+      </div>
+    );
+  }
+}
 
 export default AppBarComponent;
--- imports/components/Main/Main.jsx
+++ imports/components/Main/Main.jsx
@@ -1,49 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Body from '../Body';
 import AppBarComponent from './AppBarComponent';
-import MenuDialog from './AppBarComponent/Menu/MenuDialog';
 
 
-class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.openDialog = this.openDialog.bind(this);
-    this.hideDialog = this.hideDialog.bind(this);
+const Main = () => (
+  <div>
+    <AppBarComponent openDialog={this.openDialog} />
 
-    this.state = {
-      showMenuDialog: false,
-    };
-  }
-
-  openDialog() {
-    this.setState({
-      showMenuDialog: true,
-    });
-  }
-
-  hideDialog() {
-    this.setState({
-      showMenuDialog: false,
-    });
-  }
-
-  render() {
-    const { showMenuDialog } = this.state;
-
-    return (
-      <div>
-        <AppBarComponent openDialog={this.openDialog} />
-
-        <Body />
-
-        <MenuDialog
-          open={showMenuDialog}
-          onClose={this.hideDialog}
-        />
-      </div>
-    );
-  }
-}
+    <Body />
+  </div>
+);
 
 export default Main;
