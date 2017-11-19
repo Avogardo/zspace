97877b08f25f3c003009a992c888c2dd2ca72818
Avogardo <avogardo0@gmail.com>
2017-11-19 20:20:41 +0100

    replace google charts with chartsJS

--- imports/components/Charts/Charts.jsx
+++ imports/components/Charts/Charts.jsx
@@ -1,7 +1,6 @@
 import React, { Component, PropTypes } from 'react';
-import { Chart } from 'react-google-charts';
 import { FullPageLoader } from '/imports/components/Loaders';
-
+import { Line } from 'react-chartjs-2';
 
 class Charts extends Component {
   constructor(props) {
@@ -11,18 +10,37 @@ class Charts extends Component {
   render() {
     const { chartData } = this.props;
 
+    const data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'My First dataset',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [65, 59, 80, 81, 56, 55, 40]
+        }
+      ]
+    };
+
     return (
       <div className="tile shadow">
         {chartData ?
-          <Chart
-            chartType="AreaChart"
-            data={chartData}
-            options={{}}
-            graph_id="ScatterChart"
-            width="90%"
-            height="350px"
-            legend_toggle
-          />
+          <Line data={data} />
           :
           <FullPageLoader />
         }
